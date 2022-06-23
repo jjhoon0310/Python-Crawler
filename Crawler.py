7c86023b155aec36a2c03998b3430c97514c34e2
jjhoon0310 <jjhoon0310@gmail.com>
2022-06-23 10:28:57 +0900

    URL 서치 기능 추가 및 다음 이미지 크롤링

--- Crawler.py
+++ Crawler.py
@@ -29,30 +29,27 @@ site, topic, type, num, save_path = input().split()
 num = int(num)
 
 if site == "google":
-    site = "http://www.google.com"
-    name = "q"
+    url = "http://www.google.com/search?q=" + topic + "&source=lmns&bih=722&biw=1536&hl=ko&sa=X&ved=2ahUKEwj8zsz-rsH4AhUE26QKHQExCioQ_AUoAHoECAEQAA"
     if type == "img":
-        site ="https://images.google.com/"
+        url ="https://images.google.com/search?q=" + topic + "&tbm=isch&sxsrf=ALiCzsafEkKE1WFqp9nWWIXLSpccWSoB0Q%3A1655910957657&source=hp&biw=1536&bih=722&ei=LTKzYoiRJZDrsAfxzJaABA&iflsig=AJiK0e8AAAAAYrNAPTnG88ayH60tfMErSTNGSwCHvcn2&oq=두햐ㅜㄷ&gs_lcp=CgNpbWcQAxgCMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABFAAWNoIYManCmgBcAB4AYABuwKIAZcNkgEFMi01LjGYAQCgAQGqAQtnd3Mtd2l6LWltZw&sclient=img"
         img = "rg_i"
 
 elif site == "naver":
-    site = "https://www.naver.com/"
-    name = "query"
+    url = "https://www.naver.com/search.naver?where=nexearch&sm=tab_jum&query=" + topic
     if type == "img":
-        site ="https://search.naver.com/search.naver?where=image&section=image&query="
+        url ="https://search.naver.com/search.naver?where=image&section=image&query=" + topic
         img = "_image"
 
 else:
-    site = "https://www.daum.net/"
-    name = "q"
+    url = "https://www.daum.net/search?w=tot&DA=YZR&t__nil_searchbox=btn&sug=&sugo=&sq=&o=&q=" + topic
+    if type == "img":
+        url ="https://search.daum.net/search?w=img&nil_search=btn&DA=NTB&enc=utf8&q=" + topic
+        img = "thumb_img"
 
 # Opening Browser
 driver = webdriver.Chrome("C:\Python\selenium\chromedriver.exe")
 driver.implicitly_wait(3)
-driver.get(site)
-elem = driver.find_element_by_name(name)
-elem.send_keys(topic)
-elem.send_keys(Keys.RETURN)
+driver.get(url)
 
 # Input is Doc
 if type == "doc":
@@ -61,6 +58,12 @@ if type == "doc":
 
 # Input is excel
 elif type == "excel":
+    wb = Workbook()
+    wb.create_sheet(topic)
+    sheet = wb[topic]
+    sheet.append(["Title", "URL"])
+    wb.save(save_path)
+
     elem = driver.find_elements_by_class_name()
 
 # Input is image
